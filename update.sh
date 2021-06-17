5071e9ec415126a7e9e207b5224f685e16c7abea
Serge K <cmt@phnx47.net>
2021-06-17 20:20:47 +0700

    chore: change update.sh

--- update.sh
+++ update.sh
@@ -8,6 +8,8 @@ ver=${2}
 if [ -z "$pkg" ]; then echo "Please provide package!"; exit 1; fi
 if [ -z "$ver" ]; then echo "Please provide version!"; exit 1; fi
 
+nvchecker -c check.toml
+
 cd ${pkg}
 sed -E -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
