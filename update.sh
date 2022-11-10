85ec1b8648fb50c8a26ae89929e21a024fe5a178
Serge K <cmt@phnx47.net>
2022-11-10 17:36:52 +0700

    feat: improve update.sh

--- update.sh
+++ update.sh
@@ -5,21 +5,27 @@ set -e
 pkg=${1%/}
 
 if [ -z "$pkg" ]; then
-  echo "Please provide package!"
+  echo "Please provide package name!"
   exit 1
 fi
 
 nvchecker -c check.toml
-ver=$(cat new_ver.json | jq .\"${pkg}\")
+new_ver=$(cat new_ver.json | jq .\"${pkg}\")
 
-ver="${ver%\"}"
-ver="${ver#\"}"
+new_ver="${new_ver%\"}"
+new_ver="${new_ver#\"}"
 
 cd ${pkg}
+
+old_ver=$(grep -P '^pkgver' PKGBUILD | cut -d= -f2)
+[ "${old_ver}" = "${new_ver}" ] && echo "${pkg} is already at ${new_ver}" && exit 0
+[ $(vercmp "${old_ver}" "${new_ver}") -eq 1 ] && echo "The old_ver=${old_ver} is greater than new_ver=${new_ver}." && exit 1
+
 sed -E -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
 sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
 updpkgsums
-makepkg --printsrcinfo > .SRCINFO
+# 'aurpublish' auto generate .SRCINFO
+# makepkg --printsrcinfo > .SRCINFO
 makepkg
 cd ..
 
