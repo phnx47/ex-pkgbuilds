818a4c3d2f70a4b2e1e9d4faea57240809eb7491
Serge K <cmt@phnx47.net>
2022-11-10 18:18:36 +0700

    fix: update.sh

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ old_ver=$(grep -P '^pkgver' PKGBUILD | cut -d= -f2)
 [ "${old_ver}" = "${new_ver}" ] && echo "${pkg} is already at ${new_ver}" && exit 0
 [ $(vercmp "${old_ver}" "${new_ver}") -eq 1 ] && echo "The old_ver=${old_ver} is greater than new_ver=${new_ver}." && exit 1
 
-sed -E -i "s/pkgver=.*/pkgver=${ver}/" PKGBUILD
+sed -E -i "s/pkgver=.*/pkgver=${new_ver}/" PKGBUILD
 sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
 updpkgsums
 # 'aurpublish' auto generate .SRCINFO
