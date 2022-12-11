006593c85fa5d9e8860107d92c8ddf6dd9fb0eee
Serge K <cmt@phnx47.net>
2022-12-11 13:05:45 +0700

    feat: improve update.sh

--- update.sh
+++ update.sh
@@ -4,22 +4,18 @@ set -e
 
 pkg=${1%/}
 
-if [ -z "$pkg" ]; then
+if [ -z "${pkg}" ]; then
   echo "Please provide package name!"
   exit 1
 fi
 
-nvchecker -e ${pkg} -c check.toml
-new_ver=$(cat new_ver.json | jq .\"${pkg}\")
-
-new_ver="${new_ver%\"}"
-new_ver="${new_ver#\"}"
-
-cd ${pkg}
+nvchecker -e "${pkg}" -c check.toml
+new_ver=$(jq -r .\""${pkg}"\" new_ver.json)
+cd "${pkg}"
 
 old_ver=$(grep -P '^pkgver' PKGBUILD | cut -d= -f2)
 [ "${old_ver}" = "${new_ver}" ] && echo "${pkg} is already at ${new_ver}" && exit 0
-[ $(vercmp "${old_ver}" "${new_ver}") -eq 1 ] && echo "The old_ver=${old_ver} is greater than new_ver=${new_ver}." && exit 1
+[ "$(vercmp "${old_ver}" "${new_ver}")" -eq 1 ] && echo "The old_ver=${old_ver} is greater than new_ver=${new_ver}." && exit 1
 
 sed -E -i "s/pkgver=.*/pkgver=${new_ver}/" PKGBUILD
 sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
@@ -29,4 +25,4 @@ updpkgsums
 makepkg
 cd ..
 
-nvtake ${pkg} -c check.toml
+nvtake "${pkg}" -c check.toml
