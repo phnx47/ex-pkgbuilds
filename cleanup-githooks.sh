c478ecdf9963f14c3225abc9f84859a9227e8d4b
Serge K <cmt@phnx47.net>
2023-05-21 21:52:29 +0700

    feat(scripts): improve cleanup git hooks

--- cleanup-githooks.sh
+++ cleanup-githooks.sh
@@ -5,8 +5,7 @@ set -eu -o pipefail
 HOOKDIR="$(git rev-parse --git-dir)/hooks"
 
 if [ -d "${HOOKDIR}" ]; then
-  grep -lr "Created by Husky" "${HOOKDIR}" | xargs -d"\n" rm
-  echo "husky hooks removed from ${HOOKDIR}"
+  grep -lr "Created by Husky" "${HOOKDIR}" | xargs -r -d "\n" rm && echo "husky hooks removed from ${HOOKDIR}"
 else
   echo "no git hooks folder found"
   exit 1
