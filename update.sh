c363976712fc4972074492f71e37765f2a5a7933
Serge K <cmt@phnx47.net>
2023-05-21 14:57:21 +0700

    ci(scripts): improve update.sh

--- update.sh
+++ update.sh
@@ -2,26 +2,26 @@
 
 set -eu -o pipefail
 
-pkg=${1%/}
+pkgname="${1%/}"
+srcname="${pkgname%-bin}"
 
-if [ -z "${pkg}" ]; then
-  echo "Please provide package name!"
+echo "pkgname=${pkgname}"
+echo "srcname=${srcname}"
+
+if [ -z "${pkgname}" ]; then
+  echo "Please provide pkgname!"
   exit 1
 fi
 
-nvchecker -e "${pkg}" -c check.toml
-new_ver=$(jq -r .\""${pkg}"\" new_ver.json)
-cd "${pkg}"
+nvchecker -e "${srcname}" -c check.toml
+nver=$(jq -r .\""${srcname}"\" nver.json)
+cd "${pkgname}"
 
-old_ver=$(grep -P '^pkgver' PKGBUILD | cut -d= -f2)
-[ "${old_ver}" = "${new_ver}" ] && echo "${pkg} is already at ${new_ver}" && exit 0
-[ "$(vercmp "${old_ver}" "${new_ver}")" -eq 1 ] && echo "The old_ver=${old_ver} is greater than new_ver=${new_ver}." && exit 1
-
-sed -E -i "s/pkgver=.*/pkgver=${new_ver}/" PKGBUILD
+sed -E -i "s/pkgver=.*/pkgver=${nver}/" PKGBUILD
 sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
 updpkgsums
 makepkg
 makepkg --printsrcinfo > .SRCINFO
 cd ..
 
-nvtake "${pkg}" -c check.toml
+nvtake "${srcname}" -c check.toml
