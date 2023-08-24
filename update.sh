58d041f57bfe3feefaa8811785143436d4f6a636
Serge K <cmt@phnx47.net>
2023-08-24 20:15:41 +0700

    ci(scripts): github api

--- update.sh
+++ update.sh
@@ -15,13 +15,29 @@ fi
 
 nvchecker -e "${srcname}" -c check.toml
 nver=$(jq -r .\""${srcname}"\" nver.json)
-cd "${pkgname}"
+sed -E -i "s/pkgver=.*/pkgver=${nver}/" "${pkgname}/PKGBUILD"
+sed -E -i "s/pkgrel=.*/pkgrel=1/" "${pkgname}/PKGBUILD"
 
-sed -E -i "s/pkgver=.*/pkgver=${nver}/" PKGBUILD
-sed -E -i "s/pkgrel=.*/pkgrel=1/" PKGBUILD
+src=$(taplo get -f check.toml "${srcname}".source)
+
+if [ "${src}" = "github" ] && grep -q "_sha=" "${pkgname}/PKGBUILD"; then
+  echo "GitHub source with '_sha'. Using GitHub API"
+  repo=$(taplo get -f check.toml "${srcname}".github)
+  vprefix=$(taplo get -f check.toml "${srcname}".prefix)
+  sha=$(curl "https://api.github.com/repos/${repo}/commits/${vprefix}${nver}" | jq -r '.sha' | head -c 7)
+  sed -E -i "s/_sha=.*/_sha='${sha}'/" "${pkgname}/PKGBUILD"
+fi
+
+if [ "${pkgname}" = "ledger-live" ]; then
+  echo "Using GitHub API"
+  sha=$(curl "https://api.github.com/repos/LedgerHQ/ledger-live/commits/@ledgerhq/live-desktop@${nver}" | jq -r '.sha' | head -c 7)
+  sed -E -i "s/_sha=.*/_sha='${sha}'/" "${pkgname}/PKGBUILD"
+fi
+
+cd "${pkgname}"
 updpkgsums
 makepkg -scC
-makepkg --printsrcinfo > .SRCINFO
+makepkg --printsrcinfo >.SRCINFO
 cd ..
 
 nvtake "${srcname}" -c check.toml
