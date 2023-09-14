ae342a309b0415669631e31fe6a837b96199ccc4
Serge K <cmt@phnx47.net>
2023-09-14 18:56:27 +0700

    ci(scripts): fix update _sha

--- update.sh
+++ update.sh
@@ -25,13 +25,13 @@ if [ "${src}" = "github" ] && grep -q "_sha=" "${pkgname}/PKGBUILD"; then
   repo=$(taplo get -f check.toml "${srcname}".github)
   vprefix=$(taplo get -f check.toml "${srcname}".prefix)
   sha=$(curl "https://api.github.com/repos/${repo}/commits/${vprefix}${nver}" | jq -r '.sha')
-  sed -E -i "s/_sha=.*/_sha='${sha}'/" "${pkgname}/PKGBUILD"
+  sed -i "0,/_sha='.*'/s//_sha='${sha}'/" "${pkgname}/PKGBUILD"
 fi
 
 if [ "${pkgname}" = "ledger-live" ]; then
   echo "Using GitHub API"
   sha=$(curl "https://api.github.com/repos/LedgerHQ/ledger-live/commits/@ledgerhq/live-desktop@${nver}" | jq -r '.sha')
-  sed -E -i "s/_sha=.*/_sha='${sha}'/" "${pkgname}/PKGBUILD"
+  sed -i "0,/_sha='.*'/s//_sha='${sha}'/" "${pkgname}/PKGBUILD"
 fi
 
 cd "${pkgname}"
